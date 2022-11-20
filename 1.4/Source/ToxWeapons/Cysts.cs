f20dceec114277ebda5f38d3efb04f1abcfffa43
SmArtKar <smart.karpov@gmail.com>
2022-11-20 22:00:03 +0300

    cum

---
+++ 1.4/Source/ToxWeapons/Cysts.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+using UnityEditor;
+using Mono.Unix.Native;
+
+namespace ToxWeapons
+{
+    public class Hediff_ToxicCysts : HediffWithComps
+    {
+        public override void Notify_PawnKilled()
+        {
+            base.Notify_PawnKilled();
+            GasUtility.AddGas(pawn.PositionHeld, pawn.MapHeld, GasType.ToxGas, 4f * Severity);
+        }
+
+        public override void Notify_KilledPawn(Pawn victim, DamageInfo? dinfo)
+        {
+            base.Notify_KilledPawn(victim, dinfo);
+        }
+    }
+}
